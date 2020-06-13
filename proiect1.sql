def9871f55f98987ba6fc47fcf340b1c136f485f
endriu3314 <andrei.ar1313@gmail.com>
2020-06-13 22:25:28 +0300

    revert

--- proiect1.sql
+++ proiect1.sql
@@ -1,9 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Gazdă: 127.0.0.1
+-- Timp de generare: iun. 12, 2020 la 12:03 PM
+-- Versiune server: 10.4.11-MariaDB
+-- Versiune PHP: 7.4.6
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- TABLE 'CATEGORIES'
+-- Bază de date: `proiect1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structură tabel pentru tabel `categories`
 --
 
 CREATE TABLE `categories` (
@@ -13,12 +34,10 @@ CREATE TABLE `categories` (
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+-- --------------------------------------------------------
 
 --
--- TABLE 'POSTS'
+-- Structură tabel pentru tabel `posts`
 --
 
 CREATE TABLE `posts` (
@@ -30,10 +49,42 @@ CREATE TABLE `posts` (
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Indexuri pentru tabele eliminate
+--
+
+--
+-- Indexuri pentru tabele `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexuri pentru tabele `posts`
+--
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `category_id` (`category_id`);
+
+--
+-- AUTO_INCREMENT pentru tabele eliminate
+--
+
+--
+-- AUTO_INCREMENT pentru tabele `categories`
+--
+ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pentru tabele `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constrângeri pentru tabele eliminate
+--
 
 --
 -- Constrângeri pentru tabele `posts`
@@ -41,3 +92,7 @@ ALTER TABLE `posts`
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
